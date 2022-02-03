e2b47da3cbd0a034f0931c239c472424f975a23f
Eddy Vinck <eddy.vinck@isaac.nl>
2022-02-03 13:01:55 +0100

    feat: data fetching

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,24 @@
+import { Joke } from "@prisma/client";
+import { LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = Joke;
+export const loader: LoaderFunction = async ({ params }) => {
+  const { jokeId } = params;
+  const joke = await db.joke.findUnique({
+    where: { id: jokeId },
+  });
+  if (!joke) throw new Error("Joke not found");
+  const data: LoaderData = joke;
+  return data;
+};
+
 export default function JokeRoute() {
+  const joke = useLoaderData<LoaderData>();
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{joke.content}</p>
     </div>
   );
 }
--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,24 @@
+import { Joke } from "@prisma/client";
+import { LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = Joke;
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber, // skip towards the chosen row
+  });
+  return randomJoke;
+};
+
 export default function JokesIndexRoute() {
+  const joke = useLoaderData<LoaderData>();
   return (
     <div>
       <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>{joke.content}</p>
     </div>
   );
 }
